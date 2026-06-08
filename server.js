f56e928a813d3db8ae42c2417a908f217fe6f1ae
kirill_vish <vishneakov.kirill@gmail.com>
2026-06-08 17:01:51 +0300

    fix cors 2.0

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config()
 const app = express()
 app.use(
   cors({
-    origin: "http://localhost:4200",
+    origin: "https://standart-client.vercel.app/",
     credentials: true,
   })
 )
@@ -25,12 +25,6 @@ app.use("/appointments", appointmentRoutes)
 app.use("/schedule", scheduleRoutes)
 app.use("/reports", reportRoutes)
 app.use("/users", userRoutes)
-
-app.use(
-  cors({
-    origin: ["https://standart-client.vercel.app/"],
-  })
-)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
