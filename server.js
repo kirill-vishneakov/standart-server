142cdc5edbc5b8d01a5ba311e7de37091f1bb007
kirill_vish <vishneakov.kirill@gmail.com>
2026-06-08 17:36:39 +0300

    fix db1.1

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import userRoutes from "./routes/user.js"
 import { PrismaClient } from "@prisma/client"
 
 dotenv.config()
-
+const prisma = new PrismaClient()
 const app = express()
 app.use(
   cors({
